215f6830095b53b901ff1b73392a865dbbfa5f3e
ivancehov <work@ivanchekhov.ru>
2024-12-04 13:27:27 +0300

    🖋️ Added info abour project

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,13 @@ from thoughts.router import router as thoughtsRouter
 from thoughts.models import ThoughtModel
 from models import db
 
-app = FastAPI()
+app = FastAPI(title="Anxious basket",
+              description="API сервиса для применения техники \"Worry Time\"",
+              version="1.0.0",
+              openapi_tags=[
+                  {"name": "thoughts", "description": "Операции с мыслями"}
+              ]
+              )
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,4 +20,4 @@ async def lifespan(app: FastAPI):
     
 
 app.router.lifespan_context = lifespan
-app.include_router(thoughtsRouter, prefix="/thoughts")
+app.include_router(thoughtsRouter, prefix="/thoughts", tags=["thoughts"])
